037b4dc3c7729482a09a9f33c36046429c98cbb8
Kabir058 <riduanulkabir01@gmail.com>
2025-06-07 02:50:40 +0600

    practicing object crieteria in javascript

---
+++ object.js
@@ -0,0 +1,14 @@
+let student = {
+    name : 'Kabir',
+    id : 1111058,
+    adress : 'chawkbazar,chittagong',
+    isSingle : true,
+    friends : ['Neel', 'Rafi', 'Neela', 'Sonia'],
+    car : {
+        name : 'Tesla',
+        price : 2000034,
+        model : 2025
+    }
+
+}
+console.log(student)
