aa0f90994f0ec44082af316914503e86374fb084
Kabir058 <riduanulkabir01@gmail.com>
2025-06-07 22:19:39 +0600

    add different features like array in object

--- object.js
+++ object.js
@@ -4,10 +4,16 @@ let student = {
     adress : 'chawkbazar,chittagong',
     isSingle : true,
     friends : ['Neel', 'Rafi', 'Neela', 'Sonia'],
+    movies: [{name :'no.1', year : 2015},{name : 'King khan', year : 2018}],
     car : {
         name : 'Tesla',
         price : 2000034,
         model : 2025
+        manufacturer : {
+            name : 'Tesla',
+            ceo : 'Elon mask',
+            country : 'USA'
+        }
     }
 
 }
